e72f337c36a1021b4d281b86b7eb0f00b25ab705
llDimAssll <101548861+llDimAssll@users.noreply.github.com>
2023-03-13 14:02:33 +0300

    d

---
+++ ClassLibrary/Group.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Special { get; set; }
+        public int ClassRoom { get; set; }
+        public int SubGroup { get; set; }
+        public int StartYear { get; set; }
+
+        public static void CreateGroups()
+        {
+            List<string> list = new List<string>();
+            list.Add("П");
+            list.Add("Б");
+            list.Add("Э");
+            list.Add("С");
+            list.Add("А");
+            list.Add("Г");
+            using (var db = new DBContext())
+            {
+                foreach (string s in list)
+                {
+                    Special special = new Special { Code = s, Name = "Программисты" };
+                    db.Specials.Add(special);
+                    for (int y = 0; y < 4; y++)
+                    {
+                        for (int sg = 0; sg < 2; sg++)
+                        {
+                            Group group = new Group
+                            {
+                                ClassRoom = 9,
+                                SubGroup = sg + 1,
+                                StartYear = 2019 + y,
+                                Special = special.Code
+                            };
+                            db.Groups.Add(group);
+                        }
+                    }
+                    db.SaveChanges();
+                }
+            }
+        }
+        public string GetCode()
+        {
+            int kourse = DateTime.Now.Year - StartYear;
+            if (DateTime.Now.Month >= 9) kourse++;
+            
+            return $"{kourse}-{SubGroup}{Special}{ClassRoom}";
+        }
+    }
+}
---
+++ ClassLibrary/Migrations/202303081701024_nae.cs
@@ -0,0 +1,51 @@
+﻿namespace ClassLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class nae : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Groups",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Special = c.String(),
+                        ClassRoom = c.Int(nullable: false),
+                        SubGroup = c.Int(nullable: false),
+                        StartYear = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Specials",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Code = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Login = c.String(),
+                        PasswordHash = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Specials");
+            DropTable("dbo.Groups");
+        }
+    }
+}
